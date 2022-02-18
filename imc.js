d575829f8c116908e178d802c784da67390b1915
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-02-18 18:17:07 -0300

    Finalizado. Projeto concluído desde sua estrutura até sua estilização e funcionalidade. Agradecimentos ao professor Fernando Leonid. Próximo passo: Utilizar boas práticas com o mesmo projeto.

--- imc.js
+++ imc.js
@@ -11,9 +11,24 @@ function imc(){
 
     if(nome !== '' && altura !== '' && peso !== ''){
         
-        const valorIMC = (peso / (altura * altura));
+        const valorIMC = (peso / (altura * altura) ).toFixed(2);
 
-        resultado.textContent = valorIMC;
+        let classificacao = '';
+        if(valorIMC < 18.5){
+            classificacao = 'abaixo do peso. Cuide-se.'
+        }else if(valorIMC < 25){
+            classificacao = 'com peso ideal. Mantenha-se.'
+        }else if(valorIMC < 30){
+            classificacao = ' levemente acima do peso. Atenção!'
+        }else if(valorIMC < 35){
+            classificacao = ' com obesidade grau I. Cuide-se.'
+        }else if(valorIMC < 40){
+            classificacao = ' com obesidade grau II. Cuide-se.'
+        }else{
+            classificacao = 'e você está com obesidade grau III. Cuide-se.'
+        }    
+
+        resultado.textContent = `${nome} , seu IMC é ${valorIMC} e você está ${classificacao}`;
 
     }else{
         resultado.textContent = 'Preencha todos os campos!'
@@ -21,5 +36,5 @@ function imc(){
 
 }
 
-//2ne: add.EventListener. Obejtivo: fazer com que o programa execute a função criada (imc) através do clique.
+//2nd: add.EventListener. Obejtivo: fazer com que o programa execute a função criada (imc) através do clique.
 calcular.addEventListener('click', imc);
